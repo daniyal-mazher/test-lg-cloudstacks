3bf0cdbda2b425670bd61f87ee93f60487463b92
daniyal-mazher <163314495+daniyal-mazher@users.noreply.github.com>
2025-01-29 15:47:37 +0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,14 +1,9 @@
+import html from "./postprocessing.html";
+
 export default {
-    async fetch(request) {
-        return new Response(
-            `<!DOCTYPE html>
-            <html>
-            <head><title>Post Processing</title></head>
-            <body><h1>Hello from Cloudflare Workers!</h1></body>
-            </html>`,
-            {
-                headers: { "Content-Type": "text/html" },
-            }
-        );
-    }
+  async fetch(request) {
+    return new Response(html, {
+      headers: { "Content-Type": "text/html" },
+    });
+  },
 };
