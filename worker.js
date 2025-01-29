0bda6b55383fdacecd45472f9859da59f7d15294
daniyal-mazher <163314495+daniyal-mazher@users.noreply.github.com>
2025-01-29 16:14:03 +0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,9 +1,20 @@
-import html from "./postprocessing.html";
+import postprocessing from "./postprocessing.html";
+import page2 from "./5929.html";
 
 export default {
   async fetch(request) {
-    return new Response(html, {
-      headers: { "Content-Type": "text/html" },
-    });
+    const url = new URL(request.url);
+
+    if (url.pathname === "/postprocessing") {
+      return new Response(postprocessing, {
+        headers: { "Content-Type": "text/html" },
+      });
+    } else if (url.pathname === "/page2") {
+      return new Response(page2, {
+        headers: { "Content-Type": "text/html" },
+      });
+    } else {
+      return new Response("<h1>404 - Page Not Found</h1>", { status: 404 });
+    }
   },
-};
+}
